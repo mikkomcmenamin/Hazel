73c4360e414dfca49115a0aa1c1b116fcf0e9549
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2018-12-05 19:27:43 +0200

    Add Logging

--- Hazel/Hazel/src/Hazel/EntryPoint.h
+++ Hazel/Hazel/src/Hazel/EntryPoint.h
@@ -6,7 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine\n");
+	Hazel::Log::Init();
+	Hazel::Log::GetCoreLogger()->warn("Initialized Log!");
+	Hazel::Log::GetClientLogger()->info("Hello");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
